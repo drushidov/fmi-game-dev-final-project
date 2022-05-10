2045829bc9f9658687b970718eeeb7c21efcd5db
Daniel Rushidov <daniel.rushidov@gmail.com>
2022-05-10 20:18:38 +0300

    Make player turn faster

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.AI;
 
-public class PlayerController : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
     private Camera cam;
     private NavMeshAgent agent;
