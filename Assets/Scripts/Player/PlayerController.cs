7d0a5105bdd6dc534ca87a49b9f5f82e740b9132
Daniel Rushidov <daniel.rushidov@gmail.com>
2022-05-10 20:09:09 +0300

    Add initial player movement functionality

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    private Camera cam;
+    private NavMeshAgent agent;
+
+    private void Start()
+    {
+        cam = Camera.main;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+}
