2935f71046e9705de0f74d531b382652bfc4edae
Daniel Rushidov <daniel.rushidov@gmail.com>
2022-05-12 16:29:15 +0300

    Stop enemy chasing after player dies

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -10,6 +10,33 @@ public class EnemyController : MonoBehaviour
 
     private bool canAttack = true;
 
+    private Animator animator;
+    private GameObject player;
+    private PlayerHealth playerHealth;
+
+    private void Awake()
+    {
+        animator = GetComponentInChildren<Animator>();
+        player = GameObject.FindWithTag("Player");
+        playerHealth = player.GetComponent<PlayerHealth>();
+    }
+
+    private void OnEnable()
+    {
+        playerHealth.OnDeath += OnPlayerDeath;
+    }
+
+    private void OnDisable()
+    {
+        playerHealth.OnDeath -= OnPlayerDeath;
+    }
+
+    private void OnPlayerDeath()
+    {
+        animator.SetBool("playerDetected", false);
+        animator.SetTrigger("playerDied");
+    }
+
     public bool CanAttack()
     {
         return canAttack;
