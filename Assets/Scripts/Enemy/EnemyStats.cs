10b084aff2b343e09f6e713f223e7d9e10a30832
Daniel Rushidov <daniel.rushidov@gmail.com>
2022-05-12 21:34:00 +0300

    Add enemy stats

---
+++ Assets/Scripts/Enemy/EnemyStats.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStats : MonoBehaviour
+{
+    public int level;
+    public int baseXpOnKill;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
