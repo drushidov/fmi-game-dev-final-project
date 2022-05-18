a1f390c9688f1458271626536e7803ba3aa3cb67
Daniel Rushidov <daniel.rushidov@gmail.com>
2022-05-18 17:42:47 +0300

    Add quit game option to main menu

--- Assets/Scripts/General/Manager.cs
+++ Assets/Scripts/General/Manager.cs
@@ -8,20 +8,13 @@ public class Manager : MonoBehaviour
 {
     public SceneAsset arenaScene;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void LoadArenaScene()
     {
         SceneManager.LoadScene(arenaScene.name);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
