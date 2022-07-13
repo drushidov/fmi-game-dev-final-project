02d1774101119143f07c90602d3f66e46dd29ea7
Daniel Rushidov <daniel.rushidov@gmail.com>
2022-07-13 20:59:13 +0300

    Make enemies alert nearby ones for player when hit

--- Assets/Scripts/Enemy/EnemyPlayerDetector.cs
+++ Assets/Scripts/Enemy/EnemyPlayerDetector.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class EnemyPlayerDetector : MonoBehaviour
 {
     private EnemyHealth enemyHealth;
+    private float nearbyEnemiesAlertRadius = 7.0f;
 
     void Awake()
     {
@@ -14,6 +15,21 @@ public class EnemyPlayerDetector : MonoBehaviour
     public void PlayerDetected()
     {
         transform.parent.GetComponentInChildren<Animator>().SetBool("playerDetected", true);
+    }
+
+    public void AlertNearbyEnemiesForPlayer()
+    {
+        Collider[] enemyColliders = Physics.OverlapSphere(transform.position, nearbyEnemiesAlertRadius, LayerMask.GetMask("Enemy"));
+
+        foreach (var enemyCollider in enemyColliders)
+        {
+            EnemyPlayerDetector enemyPlayerDetector = enemyCollider.gameObject.GetComponentInChildren<EnemyPlayerDetector>();
+
+            if (enemyPlayerDetector != null)
+            {
+                enemyPlayerDetector.PlayerDetected();
+            }
+        }
     }
 
     private void OnEnable()
@@ -29,6 +45,7 @@ public class EnemyPlayerDetector : MonoBehaviour
     void OnDamageTaken(float damage, float currentHealth)
     {
         PlayerDetected();
+        AlertNearbyEnemiesForPlayer();
     }
 
     private void OnTriggerEnter(Collider other)
